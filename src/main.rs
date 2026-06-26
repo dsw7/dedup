eb84a30bdc7a6b22fe070adce2d9c26c7c2c4b1e
dsw7 <dds3443@hotmail.com>
2026-06-26 00:17:29 -0700

    Check if path exists

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use std::env;
+use std::path::Path;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let loc_duplicates = if args.len() > 1 { &args[1] } else { "." };
+    let loc_duplicates = if args.len() > 1 {
+        Path::new(&args[1])
+    } else {
+        Path::new(".")
+    };
 
-    println!("Path set to: {}", loc_duplicates);
+    if !loc_duplicates.exists() {
+        eprintln!("The path {:?} does not exist.", loc_duplicates);
+        process::exit(1);
+    }
+
+    println!("Path set to: {}", loc_duplicates.display());
 }
