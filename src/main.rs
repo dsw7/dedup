7c8860bd04f7d799068ee4494d8f4b4dee27aac9
dsw7 <dds3443@hotmail.com>
2026-06-26 00:00:38 -0700

    Accept path from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let loc_duplicates = if args.len() > 1 { &args[1] } else { "." };
+
+    println!("Path set to: {}", loc_duplicates);
 }
