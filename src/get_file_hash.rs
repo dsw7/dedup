8ea7bfecfb9d896ab372a47e0883b074a53a6b7d
dsw7 <dds3443@hotmail.com>
2026-06-27 18:51:17 -0700

    Remove md5

--- src/get_file_hash.rs
+++ src/get_file_hash.rs
@@ -1,29 +1,9 @@
-use md5::Context;
 use sha2::{Digest, Sha256};
 use std::fs::File;
 use std::io::{BufReader, Read, Result};
 use std::path::Path;
 
 const CHUNK_BUF_SIZE: usize = 65536;
-
-pub fn compute_file_md5(file_path: &Path) -> Result<String> {
-    let file = File::open(file_path)?;
-
-    let mut reader = BufReader::new(file);
-    let mut context = Context::new();
-    let mut buffer = [0u8; CHUNK_BUF_SIZE];
-
-    loop {
-        let bytes_read = reader.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break; // eof
-        }
-        context.consume(&buffer[..bytes_read]);
-    }
-
-    let digest = context.compute();
-    Ok(format!("{:x}", digest))
-}
 
 pub fn compute_file_sha256<P: AsRef<Path>>(path: P) -> Result<String> {
     let file = File::open(path)?;
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ mod get_file_hash;
 mod locate_duplicates;
 mod types;
 
-use locate_duplicates::{
-    compute_md5_hashes, compute_sha256_hashes, delete_duplicate_files, isolate_duplicate_files,
-};
+use locate_duplicates::{compute_sha256_hashes, delete_duplicate_files, isolate_duplicate_files};
 use types::TypeHashes;
 
 use clap::Parser;
@@ -16,29 +14,16 @@ use std::process;
 struct Cli {
     #[arg(value_name = "DIR", default_value = ".")]
     loc_duplicates: PathBuf,
-
-    #[arg(short, long)]
-    use_sha256: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let mut hashes: TypeHashes = if cli.use_sha256 {
-        match compute_sha256_hashes(&cli.loc_duplicates) {
-            Ok(hashes) => hashes,
-            Err(error) => {
-                eprintln!("{}", error);
-                process::exit(1);
-            }
-        }
-    } else {
-        match compute_md5_hashes(&cli.loc_duplicates) {
-            Ok(hashes) => hashes,
-            Err(error) => {
-                eprintln!("{}", error);
-                process::exit(1);
-            }
+    let mut hashes: TypeHashes = match compute_sha256_hashes(&cli.loc_duplicates) {
+        Ok(hashes) => hashes,
+        Err(error) => {
+            eprintln!("{}", error);
+            process::exit(1);
         }
     };
 
