d97f06179b0e6eb2ebe1139e07d7a9eb6f5fd885
dsw7 <dds3443@hotmail.com>
2026-06-29 02:22:13 -0700

    Ignore all extensions except

--- src/locate_duplicates.rs
+++ src/locate_duplicates.rs
@@ -1,5 +1,6 @@
 use sha2::{Digest, Sha256};
 use std::collections::HashMap;
+use std::ffi;
 use std::fs::{self, File};
 use std::io::{self, Read};
 use std::path;
@@ -27,6 +28,15 @@ fn compute_file_sha256<P: AsRef<path::Path>>(path: P) -> io::Result<String> {
     Ok(format!("{:x}", hash_result))
 }
 
+fn is_valid_file_type(file: &std::path::PathBuf) -> bool {
+    static VALID_EXTENSIONS: [&str; 2] = ["png", "jpg"];
+
+    match file.extension().and_then(ffi::OsStr::to_str) {
+        Some(ext) => VALID_EXTENSIONS.contains(&ext),
+        None => false,
+    }
+}
+
 fn append_hash(hashes: &mut TypeHashes, hash: String, file: path::PathBuf) {
     hashes.entry(hash.clone()).or_default().push(file);
 }
@@ -42,11 +52,18 @@ pub fn compute_sha256_hashes(dir: path::PathBuf) -> io::Result<TypeHashes> {
         let entry = entry?;
         let metadata = entry.metadata()?;
 
-        if metadata.is_file() {
-            let filepath = entry.path();
-            let hash = compute_file_sha256(&filepath)?;
-            append_hash(&mut hashes, hash, filepath);
+        if !metadata.is_file() {
+            continue;
         }
+
+        let filepath = entry.path();
+
+        if !is_valid_file_type(&filepath) {
+            continue;
+        }
+
+        let hash = compute_file_sha256(&filepath)?;
+        append_hash(&mut hashes, hash, filepath);
     }
 
     isolate_duplicate_files(&mut hashes);
