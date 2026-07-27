3af7e9413facd79f5aaeec613e86ab7823b5f548
dsw7 <dds3443@hotmail.com>
2026-07-26 23:18:43 -0700

    Isolate image files

--- src/locate_duplicates.rs
+++ src/locate_duplicates.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::io;
 use std::path::PathBuf;
 
-fn locate_files(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
+fn locate_all_files(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
     let entries = fs::read_dir(dir)?;
 
     let mut files = Vec::new();
@@ -20,13 +20,27 @@ fn locate_files(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
     Ok(files)
 }
 
+fn isolate_image_files(files: Vec<PathBuf>) -> Vec<PathBuf> {
+    static VALID_EXTENSIONS: [&str; 7] = ["png", "jpg", "jpeg", "gif", "bmp", "tiff", "webp"];
+
+    files
+        .into_iter()
+        .filter(|file| {
+            file.extension()
+                .map_or(false, |ext| VALID_EXTENSIONS.iter().any(|&e| ext == e))
+        })
+        .collect()
+}
+
 pub fn locate_duplicates(dir: &PathBuf) -> Result<(), DeduplicationError> {
-    let files = match locate_files(dir) {
+    let files = match locate_all_files(dir) {
         Ok(files) => files,
         Err(e) => return Err(DeduplicationError(format!("{e}"))),
     };
 
-    for file in files {
+    let image_files = isolate_image_files(files);
+
+    for file in image_files {
         println!("{}", file.display());
     }
 
