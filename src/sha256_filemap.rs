55713af4df70572e6fe46d0b31f705f1bdb94091
dsw7 <dds3443@hotmail.com>
2026-07-27 02:03:14 -0700

    Remove files

--- src/sha256_filemap.rs
+++ src/sha256_filemap.rs
@@ -2,20 +2,3 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 
 pub type HashToFiles = HashMap<String, Vec<PathBuf>>;
-
-pub fn upsert_hash_and_filepath(hash_to_files: &mut HashToFiles, hash: String, filepath: PathBuf) {
-    hash_to_files.entry(hash).or_default().push(filepath);
-}
-
-pub fn isolate_duplicates(hash_to_files_all: HashToFiles) -> HashToFiles {
-    let hash_to_files_dupes: HashToFiles = hash_to_files_all
-        .into_iter()
-        .filter(|(_, files)| files.len() > 1)
-        .collect();
-
-    hash_to_files_dupes
-}
-
-pub fn empty(hash_to_files: &HashToFiles) -> bool {
-    hash_to_files.len() < 1
-}
